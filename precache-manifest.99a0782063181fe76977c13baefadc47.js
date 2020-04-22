b0d25513201be50ff3616492474843f7270c4061
Rachel4858 <rachel458458@gmail.com>
2020-04-22 23:27:01 +0900

    Updates

---
+++ precache-manifest.99a0782063181fe76977c13baefadc47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9aa39e89661f882c475cc855249d299",
+    "url": "/react-clone-project/index.html"
+  },
+  {
+    "revision": "5af0cdf2efaf4a82cd76",
+    "url": "/react-clone-project/static/css/main.5c461caf.chunk.css"
+  },
+  {
+    "revision": "72ba1877f3f7e48ae826",
+    "url": "/react-clone-project/static/js/2.616151bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-clone-project/static/js/2.616151bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5af0cdf2efaf4a82cd76",
+    "url": "/react-clone-project/static/js/main.20cf6c82.chunk.js"
+  },
+  {
+    "revision": "b2b04fff94fae94a4b81",
+    "url": "/react-clone-project/static/js/runtime-main.f6a682ea.js"
+  },
+  {
+    "revision": "72bef7e8904dacd241bf9d33637ea15b",
+    "url": "/react-clone-project/static/media/me.72bef7e8.png"
+  },
+  {
+    "revision": "21a837ebd674f5e6a6e90d16925cd50b",
+    "url": "/react-clone-project/static/media/noPosterSmall.21a837eb.png"
+  }
+]);
